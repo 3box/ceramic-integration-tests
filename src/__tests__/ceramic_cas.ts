ff5b8319a5a82393c47a067664f31885e14df72e
Spencer T Brody <spencer@3box.io>
2020-12-22 17:04:11 -0500

    Revert "Incomplete attempt to add support for running a full Ceramic node"
    This reverts commit 117b74455366a178f9d90ab683ec14407d4f281a.

--- src/__tests__/ceramic_cas.ts
+++ src/__tests__/ceramic_cas.ts
@@ -29,34 +29,34 @@ describe('Ceramic<->CAS integration', () => {
 
         expect(doc.content).toEqual(initialContent)
 
-        // // Test document creation is anchored correctly
-        // const onCreateAnchor = registerChangeListener(doc)
-        // expect(doc.state.anchorStatus).toEqual(AnchorStatus.PENDING)
-        // expect(doc.state.log.length).toEqual(1)
-        // await onCreateAnchor
-        //
-        // console.log("AAAAAA")
-        // console.log(JSON.stringify(doc.state, null, 2))
-        // expect(doc.state.anchorStatus).toEqual(AnchorStatus.ANCHORED)
-        // expect(doc.state.log.length).toEqual(2)
-        //
-        // // Test document update
-        // const newContent = { bar: 'baz'}
-        // const onUpdateAnchor = registerChangeListener(doc)
-        //
-        // await doc.change(newContent)
-        // //expect(doc.content).toEqual(newContent)
-        //
-        // // Test document update is anchored correctly
-        // expect(doc.state.anchorStatus).toEqual(AnchorStatus.PENDING)
-        // //assert.eq(doc.state.log.length, 3)
-        //
-        // await onUpdateAnchor
-        // console.log("BBBBB")
-        // console.log(JSON.stringify(doc.state, null, 2))
-        //
-        // expect(doc.state.anchorStatus).toEqual(AnchorStatus.ANCHORED)
-        // expect(doc.content).toEqual(newContent)
-        // //assert.eq(doc.state.log.length, 4)
+        // Test document creation is anchored correctly
+        const onCreateAnchor = registerChangeListener(doc)
+        expect(doc.state.anchorStatus).toEqual(AnchorStatus.PENDING)
+        expect(doc.state.log.length).toEqual(1)
+        await onCreateAnchor
+
+        console.log("AAAAAA")
+        console.log(JSON.stringify(doc.state, null, 2))
+        expect(doc.state.anchorStatus).toEqual(AnchorStatus.ANCHORED)
+        expect(doc.state.log.length).toEqual(2)
+
+        // Test document update
+        const newContent = { bar: 'baz'}
+        const onUpdateAnchor = registerChangeListener(doc)
+
+        await doc.change(newContent)
+        //expect(doc.content).toEqual(newContent)
+
+        // Test document update is anchored correctly
+        expect(doc.state.anchorStatus).toEqual(AnchorStatus.PENDING)
+        //assert.eq(doc.state.log.length, 3)
+
+        await onUpdateAnchor
+        console.log("BBBBB")
+        console.log(JSON.stringify(doc.state, null, 2))
+
+        expect(doc.state.anchorStatus).toEqual(AnchorStatus.ANCHORED)
+        expect(doc.content).toEqual(newContent)
+        //assert.eq(doc.state.log.length, 4)
     })
 })
