6a38fc8bfc9fad152cda72a1b70bb97fe8699197
Spencer T Brody <spencer@3box.io>
2021-01-11 15:44:03 -0500

    feat(tests): Basic multi-node test

---
+++ src/__tests__/ceramic_ceramic.ts
@@ -0,0 +1,121 @@
+/**
+ * @jest-environment ./build/index.js
+ */
+
+import {config} from 'node-config-ts';
+import {AnchorStatus, CeramicApi, DoctypeUtils} from "@ceramicnetwork/common"
+
+declare global {
+    const ceramic: CeramicApi;
+    const ceramic2: CeramicApi;
+}
+
+async function delay(mills: number): Promise<void> {
+    await new Promise<void>(resolve => setTimeout(() => resolve(), mills))
+}
+
+const registerChangeListener = function (doc: any): Promise<void> {
+    return new Promise(resolve => {
+        doc.on('change', () => {
+            resolve()
+        })
+    })
+}
+
+const waitForAnchor = async (doc: any): Promise<void> => {
+    let onAnchorStatusChange = registerChangeListener(doc)
+
+    while (doc.state.anchorStatus == AnchorStatus.NOT_REQUESTED ||
+           doc.state.anchorStatus == AnchorStatus.PENDING ||
+           doc.state.anchorStatus == AnchorStatus.PROCESSING) {
+        console.log("Waiting for anchor status, current status: ", doc.state.anchorStatus)
+        await onAnchorStatusChange
+        onAnchorStatusChange = registerChangeListener(doc)
+    }
+    console.log("anchor status reached")
+    expect(doc.state.anchorStatus).toEqual(AnchorStatus.ANCHORED)
+}
+
+const createWithOneLoadWithTheOther = async(ceramic1, ceramic2): Promise<void> => {
+    const content = { foo: 'bar' }
+    const doc1 = await ceramic1.createDocument('tile', {content})
+    const doc2 = await ceramic2.loadDocument(doc1.id)
+    expect(doc1.content).toEqual(content)
+    expect(doc2.content).toEqual(doc1.content)
+}
+
+const updatesAreShared = async(ceramic1, ceramic2): Promise<void> => {
+    const content0 = { state: 0 }
+    const content1 = { state: 1 }
+
+    // Create initial doc state and wait for it to be anchored
+    console.log("about to create a doc!")
+    const doc1 = await ceramic1.createDocument('tile', {content: content0})
+
+    await doc1.change({content: content1})
+
+    expect(doc1.content).toEqual(content1)
+    expect(doc1.state.log.length).toEqual(2)
+
+    console.log("doc updated!")
+    console.log("docId", doc1.id.toString())
+    console.log("genesis cid", doc1.state.log[0].cid.toString())
+    console.log("update cid", doc1.state.log[1].cid.toString())
+
+    await waitForAnchor(doc1)
+    console.log("Anchored!!! anchor CID:", doc1.state.log[2].cid.toString())
+    while(true) {
+        await delay(500)
+    }
+
+
+
+
+
+
+    // await waitForAnchor(doc1)
+    // expect(doc1.content).toEqual(content1)
+    // expect(doc1.state.anchorStatus).toEqual(AnchorStatus.ANCHORED)
+    // expect(doc1.state.log.length).toEqual(3)
+    // const doc2 = await ceramic2.loadDocument(doc1.id)
+    // expect(doc2.content).toEqual(doc1.content)
+
+
+    //const doc2 = await ceramic2.loadDocument(doc1.id)
+    // await waitForAnchor(doc2)
+    // expect(DoctypeUtils.serializeState(doc2.state)).toEqual(DoctypeUtils.serializeState(doc1.state))
+
+    // Now perform updates and make sure they get shared between nodes
+    // const onDoc2ReceivesUpdate = registerChangeListener(doc2)
+    // doc1.change({content: content1})
+    // await onDoc2ReceivesUpdate
+    // expect(doc2.content).toEqual(content1)
+
+    // todo: the other direction
+    //doc2.change({content: content1})
+}
+
+describe('Ceramic<->Ceramic integration', () => {
+    jest.setTimeout(1000 * 60 * 5) // 5 minutes
+
+    test("create with one, load with the other", async () => {
+        console.log("Running test 'create with one, load with the other'")
+        await createWithOneLoadWithTheOther(ceramic, ceramic2)
+
+        if (config.rerunWithCeramicsSwapped) {
+            console.log("Re-running test 'create with one, load with the other' with ceramics swapped")
+            await createWithOneLoadWithTheOther(ceramic2, ceramic)
+        }
+    })
+
+    test.only("updates are shared", async () => {
+        console.log("Running test 'updates are shared'")
+        await updatesAreShared(ceramic, ceramic2)
+
+        if (config.rerunWithCeramicsSwapped) {
+            //console.log("Re-running test 'updates are shared' with ceramics swapped")
+            //await updatesAreShared(ceramic2, ceramic)
+        }
+    })
+
+})
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import CeramicClient from '@ceramicnetwork/http-client';
 import { config } from 'node-config-ts';
 import { Ed25519Provider } from 'key-did-provider-ed25519'
 import ipfsClient from "ipfs-http-client"
+import IPFS from "ipfs-core"
 //@ts-ignore
 import multiformats from 'multiformats/basics'
 // @ts-ignore
 import legacy from 'multiformats/legacy'
+
+import tmp from 'tmp-promise'
 
 import dagJose from 'dag-jose'
 import Ceramic, { CeramicConfig } from "@ceramicnetwork/core";
@@ -18,47 +21,99 @@ const seed = randomBytes(32)
 // Global services that are set up once and then available in all integration tests
 declare global {
     const ceramic: CeramicApi
+    const ceramic2: CeramicApi
+}
+
+const buildCeramic = async (configObj, ipfs: IpfsApi): Promise<CeramicApi> => {
+    let ceramic
+    if (configObj.mode == "client") {
+        console.log("Creating ceramic via http client")
+        ceramic = new CeramicClient(configObj.apiURL, { docSyncEnabled: true, docSyncInterval: 500 })
+    } else if (configObj.mode == "node") {
+        console.log("Creating ceramic via local node")
+        const ceramicConfig: CeramicConfig = {
+            networkName: configObj.network,
+            pubsubTopic: configObj.pubsubTopic,
+            ethereumRpcUrl: configObj.ethereumRpc,
+            anchorServiceUrl: configObj.anchorServiceAPI,
+        }
+        ceramic = await Ceramic.create(ipfs, ceramicConfig)
+    } else if (configObj.mode == "none") {
+        return null
+    } else {
+        throw new Error(`Ceramic mode "${configObj.mode}" not supported`)
+    }
+
+    const didProvider = new Ed25519Provider(seed)
+    await ceramic.setDIDProvider(didProvider)
+
+    return ceramic
+}
+
+const buildIpfs = async (configObj): Promise<IpfsApi> => {
+    let ipfs: IpfsApi
+    multiformats.multicodec.add(dagJose)
+    const format = legacy(multiformats, dagJose.name)
+    if (configObj.mode == "client") {
+        ipfs = ipfsClient({url: configObj.apiURL, ipld: {formats: [format]}})
+    } else if (configObj.mode == "node") {
+        const repoPath = (await tmp.dir()).path
+        ipfs = await IPFS.create({
+            repo: repoPath,
+            ipld: {
+                formats: [format]
+            },
+            libp2p: {
+                config: {
+                    dht: {
+                        enabled: true,
+                        clientMode: !configObj.dhtServerMode,
+                        randomWalk: false,
+                    },
+                },
+            },
+            config: {
+                Routing: {
+                    Type: configObj.dhtServerMode ? 'dhtserver' : 'dhtclient',
+                },
+            }
+        })
+    } else if (configObj.mode == "none") {
+        return null
+    } else {
+        throw new Error(`IPFS mode "${configObj.mode}" not supported`)
+    }
+
+    return ipfs
 }
 
 export default class IntegrationTestEnvironment extends NodeEnvironment {
     async setup() {
         await super.setup();
 
-         await this.buildServicesFromConfig();
+        try {
+            await this.buildServicesFromConfig();
+        } catch (e) {
+            console.error("Building services failed", e.toString())
+        }
     }
 
     async teardown() {
         // @ts-ignore
         await this.global.ceramic.close();
+        // @ts-ignore
+        await this.global.ceramic2.close();
 
         await super.teardown()
     }
 
     private async buildServicesFromConfig() {
-        let ceramic
-        if (config.ceramic.mode == "client") {
-            console.log("Creating ceramic via http client")
-            ceramic = new CeramicClient(config.ceramic.apiURL, { docSyncEnabled: true, docSyncInterval: 500 })
-        } else if (config.ceramic.mode == "node") {
-            console.log("Creating ceramic via local node")
-            multiformats.multicodec.add(dagJose)
-            const format = legacy(multiformats, dagJose.name)
-
-            const ipfs: IpfsApi = ipfsClient({url: config.ceramic.ipfsApi, ipld: { formats: [format] } })
-
-            const ceramicConfig: CeramicConfig = {
-                networkName: config.ceramic.network,
-                ethereumRpcUrl: config.ceramic.ethereumRpc,
-                anchorServiceUrl: config.ceramic.anchorServiceAPI,
-            }
-            ceramic = await Ceramic.create(ipfs, ceramicConfig)
-        } else {
-            throw new Error(`Ceramic mode "${config.ceramic.mode}" not supported`)
-        }
-
-        const didProvider = new Ed25519Provider(seed)
-        await ceramic.setDIDProvider(didProvider)
+        const ipfs = await buildIpfs(config.services.ipfs)
+        const ipfs2 = await buildIpfs(config.services.ipfs2)
+        const ceramic = await buildCeramic(config.services.ceramic, ipfs)
+        const ceramic2 = await buildCeramic(config.services.ceramic2, ipfs2)
 
         this.global.ceramic = ceramic
+        this.global.ceramic2 = ceramic2
     }
 }
