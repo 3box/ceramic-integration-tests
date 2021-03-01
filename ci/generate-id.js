91aa9cb80acf17d876d575c14217d7a8626f687d
val <8445610+valmack@users.noreply.github.com>
2021-02-28 22:39:04 -0500

    fix: add generate id script

---
+++ ci/generate-id.js
@@ -0,0 +1,12 @@
+const { uniqueNamesGenerator, adjectives, animals, colors } = require('unique-names-generator')
+
+function main() {
+  const runId = uniqueNamesGenerator({
+    dictionaries: [adjectives, animals, colors],
+    length: 3
+  })
+  console.log(runId)
+  process.exit(1)
+}
+
+main()
