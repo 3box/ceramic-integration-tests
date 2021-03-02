9b6068840af8817b804519b13239b4c33afe71e6
val <8445610+valmack@users.noreply.github.com>
2021-03-02 14:16:32 -0500

    fix: move jest config to file

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,12 @@ const {defaults} = require('jest-config')
 
 module.exports = {
     ...defaults,
-    reporters: ["default", "./jest-reporter.js"],
     testEnvironment: "node",
     transformIgnorePatterns: [
       "/node_modules(?!/did-jwt)/"
-    ]
+    ],
+    moduleNameMapper: {
+      "multiformats/basics": "multiformats/cjs/src/basics-import.js",
+      "multiformats/legacy": "multiformats/cjs/src/legacy.js"
+    }
 }
