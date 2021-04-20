dcc18329c26fff398a5d6947d2488d40056c589f
Spencer T Brody <spencer@3box.io>
2021-04-20 10:28:53 -0400

    Update jest resolver

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,5 @@ module.exports = {
     transformIgnorePatterns: [
       "/node_modules(?!/did-jwt)/"
     ],
-    moduleNameMapper: {
-      "multiformats/basics": "multiformats/cjs/src/basics-import.js",
-      "multiformats/legacy": "multiformats/cjs/src/legacy.js",
-      "multiformats/hashes/sha2": "multiformats/cjs/src/hashes/sha2.js"
-    }
+    resolver: "<rootDir>/jest-resolver.js"
 }
