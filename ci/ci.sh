2bcd1e32a16a632aa8eeb959c72e4936f6127e54
Spencer T Brody <spencer@3box.io>
2021-05-10 16:58:39 -0400

    Add some sleeps and logging to try to and stabilize ceramic_state_store test (#53)

--- ci/ci.sh
+++ ci/ci.sh
@@ -2,6 +2,7 @@
 export RUN_ID=$(node generate-id.js)
 echo "Run id: $RUN_ID"
 npm run launch:services
+sleep 5 # Give time for services to finish starting up before starting tests
 npm run test:ci
 exit_code=$?;
 node export-logs.js
