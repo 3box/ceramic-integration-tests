288e769e687617c93e517fbb649ac0596755e5ce
ukstv <sergey@ukstv.me>
2021-06-28 15:39:50 +0300

    feat: Key revocation test

---
+++ src/__tests__/key_revocation.ts
@@ -0,0 +1,95 @@
+/**
+ * @jest-environment ./build/index.js
+ */
+
+import {CeramicApi} from "@ceramicnetwork/common";
+import * as sha256 from "@stablelib/sha256";
+import * as uint8arrays from "uint8arrays";
+import KeyDidResolver from "key-did-resolver";
+import ThreeIdResolver from "@ceramicnetwork/3id-did-resolver";
+import { Resolver } from "did-resolver";
+import {DID, GeneralJWS} from "dids";
+import ThreeIdProvider from "3id-did-provider";
+import {TileDocument} from "@ceramicnetwork/stream-tile";
+import {waitForAnchor} from "../utils";
+import * as didJWT from "did-jwt";
+
+declare global {
+    const ceramic: CeramicApi;
+    const ceramicClient: CeramicApi;
+}
+
+async function extractKid(did: DID): Promise<string> {
+    const jws = await did.createJWS(`KID ${Math.random()}`);
+    const headerRaw = jws.signatures[0].protected;
+    const header = JSON.parse(
+        uint8arrays.toString(uint8arrays.fromString(headerRaw, "base64url"))
+    );
+    return header.kid as string;
+}
+
+function toGeneralJWS(jws: string): GeneralJWS {
+    const [protectedHeader, payload, signature] = jws.split('.')
+    return {
+        payload,
+        signatures: [{ protected: protectedHeader, signature }],
+    }
+}
+
+test('key revocation', async () => {
+    // 1. Setup initial keys
+    const seedString = `first-seed-${Math.random()}`
+    const seed = sha256.hash(uint8arrays.fromString(seedString));
+    const keyDidResolver = KeyDidResolver.getResolver();
+    const threeIdResolver = ThreeIdResolver.getResolver(ceramic);
+    const resolver = new Resolver({
+        ...threeIdResolver,
+        ...keyDidResolver,
+    });
+    const threeIdProvider = await ThreeIdProvider.create({
+        getPermission: async () => [],
+        authSecret: seed,
+        authId: "first",
+        ceramic: ceramicClient,
+    });
+    const did = new DID({
+        provider: threeIdProvider.getDidProvider(),
+        resolver: resolver,
+    });
+    await did.authenticate();
+    ceramicClient.did = did;
+    const firstKid = await extractKid(ceramic.did);
+    const firstSigner = threeIdProvider.keychain._keyring.getSigner();
+
+    // 2. Create tile
+    const tile = await TileDocument.create(ceramicClient, {
+        stage: "Signed by vanilla",
+    });
+    await tile.update({ stage: "Signed second time" }, undefined, {
+        anchor: true,
+    });
+    await waitForAnchor(tile, 60*60);
+
+    // 3. Rotate key
+    await threeIdProvider.keychain.add(
+        "second",
+        sha256.hash(uint8arrays.fromString(`second-seed-${Math.random()}`))
+    );
+    await threeIdProvider.keychain.remove("first");
+    await threeIdProvider.keychain.commit();
+    const didTile = await ceramicClient.loadStream(did.id.replace(`did:3:`, ''))
+    await waitForAnchor(didTile, 60 * 60)
+
+    // 3. Prepare signing with the old key
+    ceramicClient.did.createJWS = async (payload, options) => {
+        const compactJWS = await didJWT.createJWS(payload, firstSigner, {
+            kid: firstKid,
+        });
+        return toGeneralJWS(compactJWS);
+    };
+
+    // 4. This should blow
+    await expect(tile.update({ stage: "Third round" }, undefined, {
+        anchor: true,
+    })).rejects.toThrow(/JWS was signed with a revoked DID version/)
+})
