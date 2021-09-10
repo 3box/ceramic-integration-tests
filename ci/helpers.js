bb80484af0cd958cb0984cde2663584cc4e6b660
Mahendra (3Web) Hirani <mahendra3web@github.com>
2021-09-10 12:54:50 +0100

    feat: added cloudwatch logfile for discord message for report-exit

---
+++ ci/helpers.js
@@ -0,0 +1,86 @@
+const https = require('https')
+const { ECSClient, ListTasksCommand } = require('@aws-sdk/client-ecs')
+
+/**
+ * Returns kv object for Discord fields
+ * @param {Array<string>} taskArns 
+ * @returns {object}
+ */
+function generateDiscordCloudwatchLogFile(taskArns) {
+  const arnRegex = /\w+$/
+
+  const LogFiles = taskArns.map((arn, index) => {
+    let LogFileName 
+    const id = arn.match(arnRegex)
+    if (id) {
+      LogFileName = `${process.env.CLOUDWATCH_LOG_BASE_URL}${id[0]}`
+    }
+    return LogFileName 
+  })
+
+  return LogFiles 
+}
+
+
+/**
+ * Returns list of running ECS anchor tasks
+ * @returns {Array<string>}
+ */
+async function listECSTasks() {
+  const client = new ECSClient({
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+  })
+
+  const params = {
+    cluster: process.env.AWS_ECS_CLUSTER,
+    family: process.env.AWS_ECS_FAMILY
+  }
+
+  const command = new ListTasksCommand(params)
+
+  const data = await client.send(command)
+
+  if (data.$metadata.httpStatusCode > 399) {
+    throw Error(data.$metadata.httpStatusCode)
+  } else {
+    return data.taskArns
+  }
+}
+
+
+/**
+ * Sends a POST to the discord webhookUrl
+ * @param {string} webhookUrl Discord webhook url
+ * @param {any} data POST data
+ * @param {Number} retryDelayMs If -1, will not retry, otherwise the millisecond delay before 1 retry
+ */
+function sendDiscordNotification(webhookUrl, data, retryDelayMs = -1) {
+  const options = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
+  const req = https.request(webhookUrl, options, (res) => {
+    console.log(`Notification request status code: ${res.statusCode}`)
+    if (res.statusCode >= 500 && retryDelayMs > -1) {
+      console.log(`Retrying after ${retryDelayMs} milliseconds...`)
+      setTimeout(() => {
+        sendDiscordNotification(webhookUrl, data)
+      }, retryDelayMs)
+    }
+  })
+  req.on('error', console.error)
+  req.write(JSON.stringify(data))
+  req.end()
+}
+
+module.exports = {
+  generateDiscordCloudwatchLogFile,
+  listECSTasks,
+  sendDiscordNotification
+}
--- ci/report-exit.js
+++ ci/report-exit.js
@@ -1,6 +1,16 @@
+const { generateDiscordCloudwatchLogFile, listECSTasks, sendDiscordNotification } = require('./helpers')
 const child_process = require('child_process')
 
-function main() {
+async function main() {
+  const taskArns = await listECSTasks()  // like  taskArns =  [ 'arn:aws:ecs:us-east-2:967314784947:task/ceramic-dev-tests/2466935a544f47ec9a1c3d8add235c84' ]
+  const logFileName = generateDiscordCloudwatchLogFile(taskArns)
+  let logFile = logFileName
+  if (logFileName.length === 0) {
+    logFile = ["No LogFile found!!!"]
+  }
+
+  console.log("logFileName::::=",logFileName)
+  
   const message = [
     {
       title: 'Tests Exited',
@@ -10,18 +20,23 @@ function main() {
         {
           name: 'Configuration',
           value: `${process.env.NODE_ENV}`,
+        },
+        { 
+          name: 'Log file',
+          value: `${logFile}`,
         }
       ],
     },
   ]
   const data = { embeds: message, username: 'jest-reporter' }
-  const out = child_process.execSync(
-    `curl -X POST \
-        -H "Content-Type: application/json" \
-        -d '${JSON.stringify(data)}' \
-        ${process.env.DISCORD_WEBHOOK_URL}`
-  )
-  console.log(out.toString())
+  const retryDelayMs = 300000 // 300k ms = 5 mins
+  sendDiscordNotification(process.env.DISCORD_WEBHOOK_URL, data, retryDelayMs)
 }
 
-main()
+main().then(() => {
+        console.log('Done')
+      })
+      .catch((error) => {
+        console.error(error)
+        process.exit(1)
+      })
