b83b5073c3cb47e9bf127e223618438d2fb5ea30
Mahendra (3Web) Hirani <mahendra3web@github.com>
2021-09-20 17:27:53 +0100

    chore: cleanup after PR review

--- ci/report-exit.js
+++ ci/report-exit.js
@@ -2,11 +2,10 @@ const { generateDiscordCloudwatchLogFile, listECSTasks, sendDiscordNotification 
 const child_process = require('child_process')
 
 async function main() {
-  const taskArns = await listECSTasks()  // like  taskArns =  [ 'arn:aws:ecs:us-east-2:967314784947:task/ceramic-dev-tests/2466935a544f47ec9a1c3d8add235c84' ]
-  const logFileName = generateDiscordCloudwatchLogFile(taskArns)
-  let logFile = logFileName
-  if (logFileName.length === 0) {
-    logFile = ["No LogFile found!!!"]
+  const taskArns = await listECSTasks()  // like  taskArns =  [ 'arn:aws:ecs:*********:************:task/ceramic-dev-tests/2466935a544f47ec9a1c3d8add235c84' ]
+  const logUrls = generateDiscordCloudwatchLogFile(taskArns)
+  if (logUrls.length < 1) {
+    logUrls = ["No LogFile found"]
   }
 
   console.log("logFileName::::=",logFileName)
@@ -22,13 +21,14 @@ async function main() {
           value: `${process.env.NODE_ENV}`,
         },
         { 
-          name: 'Log file (Currently all test run tasks logs running shown, will fix to one relevant one in future)',
-          value: `${logFile}`,
+          name: 'Logs',
+          value: `${logUrls}`,
         }
       ],
     },
   ]
-  const data = { embeds: message, username: 'jest-reporter' }
+  const userName = 'jest-reporter'}
+  const data = { embeds: message, username: userName }
   const testPassUrl = process.env.DISCORD_WEBHOOK_URL_TEST_RESULTS
   const testFailUrl = process.env.DISCORD_WEBHOOK_URL_TEST_FAILURES
  
