9abb77246f1f3daf8736578c762c7b8035f34836
Mahendra (3Web) Hirani <mahendra3web@github.com>
2021-09-25 15:23:33 +0100

    chore: better formed async function

--- ci/report-exit.js
+++ ci/report-exit.js
@@ -1,42 +1,42 @@
 const { generateDiscordCloudwatchLogUrls, listECSTasks, sendDiscordNotification } = require('./helpers')
 
-async function main() {
-  const taskArns = await listECSTasks()  // like  taskArns =  [ 'arn:aws:ecs:*********:************:task/ceramic-dev-tests/2466935a544f47ec9a1c3d8add235c84' ]
-  const logUrls = generateDiscordCloudwatchLogUrls(taskArns)
-  if (logUrls.length < 1) {
-    logUrls = ["No log Urls found"]
+const main = async () => {
+  try {
+    const taskArns = await listECSTasks();  // like  taskArns =  [ 'arn:aws:ecs:*********:************:task/ceramic-dev-tests/2466935a544f47ec9a1c3d8add235c84' ]
+    console.log('INFO: listECSTasks taskArns:=', taskArns)
+    let logUrls = generateDiscordCloudwatchLogUrls(taskArns)
+    if (logUrls.length < 1) {
+      logUrls = ["No log Urls found"]
+    }
+  
+    const message = [
+      {
+        title: 'Tests Exited',
+        description: `Run Id: ${process.env.RUN_ID}`,
+        color: 16711712,
+        fields: [
+          {
+            name: 'Configuration',
+            value: `${process.env.NODE_ENV}`,
+          },
+          { 
+            name: 'Logs',
+            value: `${logUrls}`,
+          }
+        ],
+      },
+    ]
+    const data = { embeds: message, username: 'jest-reporter' }
+    const testResultsUrl = process.env.DISCORD_WEBHOOK_URL_TEST_RESULTS
+    const testFailuresUrl = process.env.DISCORD_WEBHOOK_URL_TEST_FAILURES
+   
+    const retryDelayMs = 300000 // 300k ms = 5 mins
+    sendDiscordNotification(testResultsUrl, data, retryDelayMs)
+    sendDiscordNotification(testFailuresUrl, data, retryDelayMs)
+  } catch (err) {
+    console.error(err);
+    process.exit(1)
   }
-
-  const message = [
-    {
-      title: 'Tests Exited',
-      description: `Run Id: ${process.env.RUN_ID}`,
-      color: 16711712,
-      fields: [
-        {
-          name: 'Configuration',
-          value: `${process.env.NODE_ENV}`,
-        },
-        { 
-          name: 'Logs',
-          value: `${logUrls}`,
-        }
-      ],
-    },
-  ]
-  const data = { embeds: message, username: 'jest-reporter' }
-  const testResultsUrl = process.env.DISCORD_WEBHOOK_URL_TEST_RESULTS
-  const testFailuresUrl = process.env.DISCORD_WEBHOOK_URL_TEST_FAILURES
- 
-  const retryDelayMs = 300000 // 300k ms = 5 mins
-  sendDiscordNotification(testResultsUrl, data, retryDelayMs)
-  sendDiscordNotification(testFailuresUrl, data, retryDelayMs)
 }
 
-main().then(() => {
-        console.log('Done')
-      })
-      .catch((error) => {
-        console.error(error)
-        process.exit(1)
-      })
+main();
