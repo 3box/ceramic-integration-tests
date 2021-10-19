73cc57c9bb9a9892862ea7224ed43f05a89603f8
Mohsin Zaidi <2236875+smrz2001@users.noreply.github.com>
2021-10-18 20:45:40 -0400

    fix: disable commit hash retrieval

--- ci/report-exit.js
+++ ci/report-exit.js
@@ -3,7 +3,7 @@ const { generateDiscordCloudwatchLogUrls, listECSTasks, sendDiscordNotification,
 const main = async () => {
   try {
     const taskArns = await listECSTasks()        // like  taskArns =  [ 'arn:aws:ecs:*********:************:task/ceramic-dev-tests/2466935a544f47ec9a1c3d8add235c84' ]
-    const commitHashes = await getCommitHashes() // like  commitHashes = "ceramic-anchor-service (333fc9afb59a) <==> ipfs-daemon (6871b7dcd27d)\n"
+    // const commitHashes = await getCommitHashes() // like  commitHashes = "ceramic-anchor-service (333fc9afb59a) <==> ipfs-daemon (6871b7dcd27d)\n"
     console.log('INFO: listECSTasks taskArns:=', taskArns)
     let logUrls = generateDiscordCloudwatchLogUrls(taskArns)
     if (logUrls.length < 1) {
