50560cad2ac55db68f15db4ab0ade742bfd2e63b
Spencer T Brody <spencer+github@3box.io>
2021-10-26 16:28:47 -0400

    fix: Try to stabilize tests by waiting for setup/teardown

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { CeramicApi, IpfsApi } from '@ceramicnetwork/common';
 import { config } from 'node-config-ts';
 
 import NodeEnvironment from 'jest-environment-node'
-import { buildCeramic, buildIpfs } from './utils';
+import {buildCeramic, buildIpfs, delay} from './utils';
 
 // Global services that are set up once and then available in all integration tests
 declare global {
@@ -27,14 +27,21 @@ export default class IntegrationTestEnvironment extends NodeEnvironment {
     async teardown() {
         // @ts-ignore
         await this.global.ceramic.close();
+        this.global.ceramic = null
+
         // @ts-ignore
         await this.global.ceramicClient.close();
+        this.global.ceramicClient = null
+
+        this.global.ipfs = null
+
+        await delay(3000) // Give some time for things to fully shut down
 
         await super.teardown()
     }
 
     private async buildServicesFromConfig() {
-        console.info(`Starting tests in environment: ${process.env.NODE_ENV}`)
+        console.info(`Starting test in environment: ${process.env.NODE_ENV}`)
 
         if (config.jest.services.ceramicClient.mode == "node") {
             throw new Error("Mode 'node' isn't supported for 'ceramicClient'")
@@ -43,6 +50,8 @@ export default class IntegrationTestEnvironment extends NodeEnvironment {
         const ipfs = await buildIpfs(config.jest.services.ipfs)
         const ceramic = await buildCeramic(config.jest.services.ceramic, ipfs)
         const ceramicClient = await buildCeramic(config.jest.services.ceramicClient)
+
+        await delay(3000) // Give some time for things to fully start up before continuing
 
         this.global.ceramic = ceramic
         this.global.ceramicClient = ceramicClient
