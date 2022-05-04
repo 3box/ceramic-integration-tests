9fa38973451e604a893bdd17094416d94fc6f626
v-stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2022-05-04 12:54:51 -0400

    Revert "feat: use dev-qa for ceramic network"
    This reverts commit 98f1d6131644af1e819e8c2e1d6f65067c823275.

--- ci/ci.sh
+++ ci/ci.sh
@@ -4,12 +4,12 @@ echo "INFO: Run id: $RUN_ID"
 echo "INFO: Sleep seconds set for services to start: ${SLEEP:=60}"  # defaults to 60 seconds
 
 if [[ $NODE_ENV == "local_client-public" ]]; then
-  $(node node_modules/@ceramicnetwork/cli/bin/ceramic daemon --verbose --log-to-files --network dev-qa --ethereum-rpc https://rinkeby.infura.io/v3/b6685df41e1647c4be0046dfa62a020b) &
+  $(node node_modules/@ceramicnetwork/cli/bin/ceramic daemon --verbose --log-to-files --network dev-unstable --ethereum-rpc https://rinkeby.infura.io/v3/b6685df41e1647c4be0046dfa62a020b) &
 fi
 
 if [[ $NODE_ENV == "local_node-private" ]]; then
   export IPFS_API_PORT=5011
-  export CERAMIC_NETWORK='dev-qa'
+  export CERAMIC_NETWORK='dev-unstable'
   export DEBUG='ipfs*error'
   $(node node_modules/@ceramicnetwork/ipfs-daemon/bin/ipfs-daemon) &
 fi
