152cfe9baca3fec93bcf4b099ed50b5caa2b40db
Golda Velez <golda@3box.io>
2022-09-14 13:35:57 -0700

    remove debugging section

--- ci/ci.sh
+++ ci/ci.sh
@@ -3,17 +3,6 @@ export RUN_ID=$(node generate-id.js)
 echo "INFO: Run id: $RUN_ID"
 echo "INFO: Sleep seconds set for services to start: ${SLEEP:=60}"  # defaults to 60 seconds
 
-# DEBUGGING START  - REMOVE ME LATER
-
-echo "INFO: Environment: ***********"
-env
-echo "******************************"
-mkdir /app/root
-mkdir /app/root/.ceramic
-mkdir /app/root/.ceramic/logs
-
-# DEBUGGING END
-
 if [[ $NODE_ENV == "local_client-public" ]]; then
   $(node node_modules/@ceramicnetwork/cli/bin/ceramic daemon --verbose --log-to-files --network dev-unstable --anchor-service-api https://cas-qa.3boxlabs.com --ethereum-rpc https://goerli.infura.io/v3/b6685df41e1647c4be0046dfa62a020b) &
 fi
